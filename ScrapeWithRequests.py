137cc58799a8b98dfc6a78cc953eb27da575a5f6
nkarman <noah.karman@pop.belmont.edu>
2017-01-17 13:15:12 -0600

    Fixed Naming error in ScrapWithRequests.py

---
+++ ScrapeWithRequests.py
@@ -0,0 +1,16 @@
+import requests
+import datetime
+
+today = str(datetime.datetime.now()).split(' ')[0]
+
+sites = {'Reverb': 'http://reverb.com/',
+         'SweetWater':'http://www.sweetwater.com/'}
+
+for name, link in sites.items():
+    response = requests.get(link)
+    html = response.content
+
+    fileName = today + '.' + name + '.html'
+    outfile = open(fileName, "wb")
+    outfile.write(html)
+    outfile.close()
