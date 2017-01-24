aee4fde4b533c060c60b0112ffc7e66ac0a541ae
nkarman <noah.karman@pop.belmont.edu>
2017-01-24 15:12:11 -0600

    GuitarCenterToJSON now parses for every page of used gear on guitar center's store

--- GuitarCenterToJSON.py
+++ GuitarCenterToJSON.py
@@ -9,49 +9,69 @@ today = str(datetime.datetime.now().date())
 # Create a list of dictionaries for JSON Object
 response = []
 
-# Scrape GuitarCenter with requests
+# Create a list of urls, and perform parsing for every URL
+URLs = []
+
+# Scrape GuitarCenter first used gear page for total number of products for sale
 gcUsedURL = 'http://www.guitarcenter.com/Used/'
+URLs.append(gcUsedURL)
+
 pageGCUsed = requests.get(gcUsedURL)
-
-# Prepare for parsing APNewsBriefs with BeautifulSoup
 soupGCUsed = BeautifulSoup(pageGCUsed.content, 'lxml')
+# currPageNum will always have initial value of 100, this is the required value for the second page
+currPageNum = 100
+totalProducts = soupGCUsed.find('var', class_='searchTotalResults').contents[0]
+totalProducts = int(totalProducts)
+#last page of data
+endPageNum = totalProducts-100
 
-# Parse APNewsBriefs url
-# 'position' marks the beginning of each news brief in the html
-# All other data is found in its relationship to 'position'
-for position in soupGCUsed.find_all('div', class_='productDetails'):
-    #title
-    titlePosition = position.find('div', class_='productTitle')
-    title = titlePosition.find('a').string
+#populates list of urls with every url for every page containing used gear
+while currPageNum < endPageNum:
+    currPageNum += 100
+    nextURL = 'http://www.guitarcenter.com/Used/#pageName=used-page&N=1076&Nao=' + str(currPageNum) + '&recsPerPage=100&v=g&postalCode=37212&radius=100&profileCountryCode=US&profileCurrencyCode=USD'
+    URLs.append(nextURL)
+# parse data for every URL in list
+for url in URLs:
+    pageGCUsed = requests.get(url)
+    soupGCUsed = BeautifulSoup(pageGCUsed.content, 'lxml')
+    print(url)
+    for position in soupGCUsed.find_all('div', class_='productDetails'):
+        #title
+        titlePosition = position.find('div', class_='productTitle')
+        title = titlePosition.find('a').string
 
-    #link
-    link = 'https://guitarcenter.com' + titlePosition.contents[1].contents[1].attrs['href']
+        #link
+        link = 'https://guitarcenter.com' + titlePosition.contents[1].contents[1].attrs['href']
 
-    #condition
-    conditionPosition = position.find('div', class_='productCondition')
-    condition = conditionPosition.contents[0]
+        #condition
+        conditionPosition = position.find('div', class_='productCondition')
+        condition = conditionPosition.contents[0]
 
-    #price
-    pricePosition = position.find('span', class_='productPrice')
-    price = (pricePosition.contents[2] + '.' + pricePosition.contents[3].contents[0])[:-3]
+        #price
+        pricePosition = position.find('span', class_='productPrice')
+        price = (pricePosition.contents[2] + '.' + pricePosition.contents[3].contents[0])[:-3]
 
-    #location
-    locationPosition = position.find('div', class_='storeName')
-    location = locationPosition.contents[3].string
+        #location
+        locationPosition = position.find('div', class_='storeName')
+        location = locationPosition.contents[3].string
 
-    # attempt to open productPage URL. it looks like guitarCenter is blocking me out :(
-    #productPage = requests.get(link)
-    #soupProductPage = BeautifulSoup(productPage.content, 'lxml')
+        #nextLink
+        nextLinkPosition = position.find('div', class_='searchPagination')
 
-    #categoryPosition = soupProductPage.find("a", class_='category')
-    #category = categoryPosition.contents[0]
+        # attempt to open productPage URL. it looks like guitarCenter is blocking me out :(
+        #productPage = requests.get(link)
+        #soupProductPage = BeautifulSoup(productPage.content, 'lxml')
 
-    # Make changes to response for GCUsed
-    response.append({'productTitle': title,
-                     'productCondition': condition,
-                     'productPrice': price,
-                     'productLocation': location,
-                     'productLink': link})
+        #categoryPosition = soupProductPage.find("a", class_='category')
+        #category = categoryPosition.contents[0]
+
+        # Make changes to response for GCUsed
+        response.append({'productTitle': title,
+                       'productCondition': condition,
+                        'productPrice': price,
+                        'productLocation': location,
+                        'productLink': link})
+
 
 # Write response to JSON file
 postingsFile = today + '.GCUSed.json'
